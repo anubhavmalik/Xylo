df560d467bce6732dde9ac15b23d7216fbfa3316
Anubhav <anubhavmalikdeveloper@gmail.com>
2018-04-28 21:53:34 +0530

    Initial Commit

--- Xylo/ViewController.swift
+++ Xylo/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var xylophoneSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "note1",ofType: "wav")!)
+    
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        let tag = sender.tag
+        
+        playSound(tag: tag)
+        
+    }
+    func playSound(tag : Int){
+        
+        let soundURL = Bundle.main.url(forResource: "note\(tag)", withExtension: "wav")
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch{
+            print(error)
+        }
+        audioPlayer.play()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
